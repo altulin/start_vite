11e454728cf030d850f5edcf2b77493542a2b2c0
altln <al.tulin@gmail.com>
2025-03-25 16:53:49 +0800

    fix class

--- src/widgets/layouts/ui/LayoutBase.tsx
+++ src/widgets/layouts/ui/LayoutBase.tsx
@@ -1,12 +1,14 @@
 import { Header } from "@/widgets/header";
+import clsx from "clsx";
 import { FC } from "react";
 import { Outlet } from "react-router";
+import style from "./BaseLayout.module.scss";
 
 export const BaseLayout: FC = () => {
   return (
     <>
       <Header />
-      <main className="grow">
+      <main className={clsx(style.main)}>
         <Outlet />
       </main>
     </>
